fad9d7e8c971bbe70c0947f1a3ed979fe2fed003
Michael Horan <66953737+mikehoran6@users.noreply.github.com>
2020-09-24 14:07:12 -0400

    lightingScript.js update
    Updated to be much more dynamic

--- lightingScript.js
+++ lightingScript.js
@@ -3,7 +3,11 @@
 window.onload = init;
 
 // ledCheck, redCheck, whiteCheck, blueCheck, rgbCheck, resistCheck
-var checks = [false,false,false,false,false,false];
+var filters =  ['led', 'red', 'white', 'blue', 'rgb', 'resist'];
+var checks = [];
+for (var i = 0; i < filters.length; i++){
+	checks.push(false);
+}
 
 function init() {
 	var checkBoxes = document.getElementsByClassName("cb");
@@ -14,70 +18,20 @@ function init() {
 
 function displayItems(e) {
 	var checkID = e.target.id;
-	console.log(checkID);
 	var disp = document.querySelectorAll('figure');
 	for (var i = 0; i < disp.length; i++) {
 		disp[i].style.display = "none";
 	}
-	switch (checkID) {
-	case "led":
-		checks[0] = !checks[0];
-		break;
-	case "red":
-		checks[1] = !checks[1];
-		break;
-	case "white":
-		checks[2] = !checks[2];
-		break;
-	case "blue":
-		checks[3] = !checks[3];
-		break;
-	case "rgb":
-		checks[4] = !checks[4];
-		break;
-	case "resist":
-		checks[5] = !checks[5];
-	}
-
-	if (checks[0] == true) {
-		var led = document.querySelectorAll('figure.led');
-		for (var i = 0; i < led.length; i++) {
-			led[i].style.display = "block";
-		}
-	}
-	if (checks[1] == true) {
-		var red = document.querySelectorAll('figure.red');
-		for (var i = 0; i < red.length; i++) {
-			red[i].style.display = "block";
-		}
-	}
-	if (checks[2] == true) {
-		var white = document.querySelectorAll('figure.white');
-		for (var i = 0; i < white.length; i++) {
-			white[i].style.display = "block";
-		}
-	}
-	if (checks[3] == true) {
-		var blue = document.querySelectorAll('figure.blue');
-		for (var i = 0; i < blue.length; i++) {
-			blue[i].style.display = "block";
-		}
-	}
-	if (checks[4] == true) {
-		var rgb = document.querySelectorAll('figure.rgb');
-		for (var i = 0; i < rgb.length; i++) {
-			rgb[i].style.display = "block";
-		}
-	}
-	if (checks[5] == true) {
-		var resist = document.querySelectorAll('figure.resist');
-		for (var i = 0; i < resist.length; i++) {
-			resist[i].style.display = "block";
-		}
-	}
 	var count = 0;
 	for (var i = 0; i < checks.length; i++) {
+		if (checkID == filters[i]) {
+			checks[i] = !checks[i];
+		}
+		var show = document.querySelectorAll('figure.' + filters[i]);
 		if (checks[i] == true) {
+			for (var x = 0; x < show.length; x++) {
+				show[x].style.display = "block"
+			}
 			count++;
 		}
 	}
